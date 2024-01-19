43ec9ba11b665a6378518231615a04954ae6b2f0
_woo_s.j <sjwoo1999@korea.ac.kr>
2024-01-20 08:43:26 +0900

    24.01.20. es6 first commit

---
+++ es6-module/src/libs/math.js
@@ -0,0 +1,3 @@
+export default function () {
+  console.log("src/libs/math.js 파일입니다.");
+}
---
+++ es6-module/src/libs/run.js
@@ -0,0 +1,10 @@
+// math.js
+import math from "./math.js";
+// base.exception.js
+import baseException from "../exception/base.exception.js";
+// authentication.middleware.js
+import authenticationMiddleWare from "../middleware/authentication.middleware.js";
+
+math();
+baseException();
+authenticationMiddleWare();
